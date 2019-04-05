manor1234/EXOPLAYER_RTSP

commit e8f304498fabe2d259d9b9c4e85f0cb5c7d7bffc

Remove usages of the widget package and migrate to new packages.
PiperOrigin-RevId: 241656157

demos/main/src/main/java/com/google/android/exoplayer2/demo/TrackSelectionDialog.java
@@ -20,7 +20,7 @@ import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import com.google.android.material.tabs.TabLayout;
+import android.support.design.tabs.TabLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;

demos/cast/src/main/res/layout/main_activity.xml
@@ -38,7 +38,7 @@
         android:scrollbars="vertical"
         android:fadeScrollbars="false"/>
 
-    <com.google.android.material.floatingactionbutton.FloatingActionButton android:id="@+id/add_sample_button"
+    <android.support.design.floatingactionbutton.FloatingActionButton android:id="@+id/add_sample_button"
         android:src="@drawable/ic_plus"
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"

demos/main/src/main/res/layout/track_selection_dialog.xml
@@ -25,7 +25,7 @@
       android:layout_height="0dp"
       android:layout_weight="1">
 
-    <com.google.android.material.tabs.TabLayout
+    <android.support.design.tabs.TabLayout
         android:id="@+id/track_selection_dialog_tab_layout"
         android:layout_width="match_parent"
         android:layout_height="wrap_content"

commit bca65f34c3d7979398804afdf198010685160fde

Fix ExoPlayer's demo app gradle build
Broken by https://github.com/google/ExoPlayer/commit/e8f304498fabe2d259d9b9c4e85f0cb5c7d7bffc.

PiperOrigin-RevId: 241770676

demos/main/src/main/java/com/google/android/exoplayer2/demo/TrackSelectionDialog.java
@@ -20,7 +20,7 @@ import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import android.support.design.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;