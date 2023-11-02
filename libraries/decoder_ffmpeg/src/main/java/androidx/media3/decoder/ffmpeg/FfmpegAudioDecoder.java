manor1234/EXOPLAYER_RTSP

commit ea2aa79f538a8a11d34399ddbd59c425093669d2

Format with google-java-format
Also amended Javadoc and added assertion to grow method

libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/FfmpegAudioDecoder.java
@@ -63,7 +63,8 @@ import java.util.List;
     codecName = Assertions.checkNotNull(FfmpegLibrary.getCodecName(format.sampleMimeType));
     extraData = getExtraData(format.sampleMimeType, format.initializationData);
     encoding = outputFloat ? C.ENCODING_PCM_FLOAT : C.ENCODING_PCM_16BIT;
-    outputBufferSize = outputFloat ? INITIAL_OUTPUT_BUFFER_SIZE_32BIT : INITIAL_OUTPUT_BUFFER_SIZE_16BIT;
+    outputBufferSize =
+        outputFloat ? INITIAL_OUTPUT_BUFFER_SIZE_32BIT : INITIAL_OUTPUT_BUFFER_SIZE_16BIT;
     nativeContext =
         ffmpegInitialize(codecName, extraData, outputFloat, format.sampleRate, format.channelCount);
     if (nativeContext == 0) {
@@ -106,9 +107,10 @@ import java.util.List;
     }
     ByteBuffer inputData = Util.castNonNull(inputBuffer.data);
     int inputSize = inputData.limit();
-    outputBuffer.init(inputBuffer.timeUs, outputBufferSize);
-
-    int result = ffmpegDecode(nativeContext, inputData, inputSize, outputBuffer, outputBuffer.data, outputBufferSize);
+    ByteBuffer outputData = outputBuffer.init(inputBuffer.timeUs, outputBufferSize);
+    int result =
+        ffmpegDecode(
+            nativeContext, inputData, inputSize, outputBuffer, outputData, outputBufferSize);
     if (result == AUDIO_DECODER_ERROR_OTHER) {
       return new FfmpegDecoderException("Error decoding (see logcat).");
     } else if (result == AUDIO_DECODER_ERROR_INVALID_DATA) {
@@ -135,13 +137,15 @@ import java.util.List;
       }
       hasOutputFormat = true;
     }
-    outputBuffer.data.position(0);
-    outputBuffer.data.limit(result);
+    outputData.position(0);
+    outputData.limit(result);
     return null;
   }
 
   // Called from native code
-  /** @noinspection unused*/
+  /**
+   * @noinspection unused
+   */
   private ByteBuffer growOutputBuffer(SimpleDecoderOutputBuffer outputBuffer, int requiredSize) {
     // Use it for new buffer so that hopefully we won't need to reallocate again
     outputBufferSize = requiredSize;
@@ -229,7 +233,12 @@ import java.util.List;
       int rawChannelCount);
 
   private native int ffmpegDecode(
-      long context, ByteBuffer inputData, int inputSize, SimpleDecoderOutputBuffer decoderOutputBuffer, ByteBuffer outputData, int outputSize);
+      long context,
+      ByteBuffer inputData,
+      int inputSize,
+      SimpleDecoderOutputBuffer decoderOutputBuffer,
+      ByteBuffer outputData,
+      int outputSize);
 
   private native int ffmpegGetChannelCount(long context);
 

libraries/decoder/src/main/java/androidx/media3/decoder/SimpleDecoderOutputBuffer.java
@@ -51,14 +51,16 @@ public class SimpleDecoderOutputBuffer extends DecoderOutputBuffer {
   }
 
   /**
-   * Reallocates the buffer with new size
-   * Existing data between beginning of the buffer and {@link ByteBuffer#limit} is copied to the new buffer,
-   * and {@link ByteBuffer#position} is preserved. {@link ByteBuffer#limit} is set to the new size.
-   * @param newSize New size of buffer.
+   * Grows the buffer with to a new size.
+   *
+   * <p>Existing data is copied to the new buffer, and {@link ByteBuffer#position} is preserved.
+   *
+   * @param newSize New size of the buffer.
    * @return The {@link #data} buffer, for convenience.
    */
   public ByteBuffer grow(int newSize) {
     Assertions.checkNotNull(data);
+    Assertions.checkArgument(newSize >= data.limit());
     final ByteBuffer newData = ByteBuffer.allocateDirect(newSize).order(ByteOrder.nativeOrder());
     final int restorePosition = data.position();
     data.position(0);

commit 3cb6d9b55e4ab0cdb20f8e47c3f66fc87c5c83af

Fix access to stale ByteBuffer in FfmpegAudioDecoder
The native code can now reallocate the buffer if it needs to grow
its size, so we have to reacquire a reference in the Java code to
avoid accessing a stale instance.

This fixes a bug introduced by https://github.com/androidx/media/pull/746/commits/ea2aa79f538a8a11d34399ddbd59c425093669d2.

PiperOrigin-RevId: 578799862

libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/FfmpegAudioDecoder.java
@@ -15,11 +15,12 @@
  */
 package androidx.media3.decoder.ffmpeg;
 
+import static androidx.media3.common.util.Assertions.checkNotNull;
+
 import androidx.annotation.Nullable;
 import androidx.media3.common.C;
 import androidx.media3.common.Format;
 import androidx.media3.common.MimeTypes;
-import androidx.media3.common.util.Assertions;
 import androidx.media3.common.util.ParsableByteArray;
 import androidx.media3.common.util.Util;
 import androidx.media3.decoder.DecoderInputBuffer;
@@ -59,8 +60,8 @@ import java.util.List;
     if (!FfmpegLibrary.isAvailable()) {
       throw new FfmpegDecoderException("Failed to load decoder native libraries.");
     }
-    Assertions.checkNotNull(format.sampleMimeType);
-    codecName = Assertions.checkNotNull(FfmpegLibrary.getCodecName(format.sampleMimeType));
+    checkNotNull(format.sampleMimeType);
+    codecName = checkNotNull(FfmpegLibrary.getCodecName(format.sampleMimeType));
     extraData = getExtraData(format.sampleMimeType, format.initializationData);
     encoding = outputFloat ? C.ENCODING_PCM_FLOAT : C.ENCODING_PCM_16BIT;
     outputBufferSize =
@@ -128,7 +129,7 @@ import java.util.List;
       channelCount = ffmpegGetChannelCount(nativeContext);
       sampleRate = ffmpegGetSampleRate(nativeContext);
       if (sampleRate == 0 && "alac".equals(codecName)) {
-        Assertions.checkNotNull(extraData);
+        checkNotNull(extraData);
         // ALAC decoder did not set the sample rate in earlier versions of FFmpeg. See
         // https://trac.ffmpeg.org/ticket/6096.
         ParsableByteArray parsableExtraData = new ParsableByteArray(extraData);
@@ -137,6 +138,9 @@ import java.util.List;
       }
       hasOutputFormat = true;
     }
+    // Get a new reference to the output ByteBuffer in case the native decode method reallocated the
+    // buffer to grow its size.
+    outputData = checkNotNull(outputBuffer.data);
     outputData.position(0);
     outputData.limit(result);
     return null;